298a84484b926ba8908a22fd94508b2fedd0f251
NEXT455 <nafaa07738302804@gmail.com>
2026-08-29 23:04:33 +0300

    Update interface.cpp

--- boards/smoochiee-board/interface.cpp
+++ boards/smoochiee-board/interface.cpp
@@ -3,73 +3,28 @@
 /***************************************************************************************
 ** Function name: _setup_gpio()
 ** Location: main.cpp
-** Description:   initial setup for the device
+** Description:   initial setup for the device (Minimalist Build)
 ***************************************************************************************/
-// Power handler for battery detection
-//#ifdef XPOWERS_CHIP_BQ25896
-//#include <Wire.h>
-//#include <XPowersLib.h>
-//XPowersPPM PPM;
-//#endif
 
 void _setup_gpio() {
 
-    pinMode(UP_BTN, INPUT); // Sets the power btn as an INPUT
+    pinMode(UP_BTN, INPUT);
     pinMode(SEL_BTN, INPUT);
     pinMode(DW_BTN, INPUT);
     pinMode(R_BTN, INPUT);
     pinMode(L_BTN, INPUT);
 
-    //pinMode(CC1101_SS_PIN, OUTPUT);
     pinMode(NRF24_SS_PIN, OUTPUT);
-
-    //digitalWrite(CC1101_SS_PIN, HIGH);
     digitalWrite(NRF24_SS_PIN, HIGH);
-    // Starts SPI instance for CC1101 and NRF24 with CS pins blocking communication at start
 
-    bruceConfigPins.rfModule = CC1101_SPI_MODULE;
     bruceConfigPins.irRx = RXLED;
-    Wire.setPins(GROVE_SDA, GROVE_SCL);
-    // Wire.begin();
-    bool pmu_ret = false;
-   // Wire.begin(GROVE_SDA, GROVE_SCL);
-  //  pmu_ret = PPM.init(Wire, GROVE_SDA, GROVE_SCL, BQ25896_SLAVE_ADDRESS);
-  //  if (pmu_ret) {
-       // PPM.setSysPowerDownVoltage(3300);
-      //  PPM.setInputCurrentLimit(3250);
-        //Serial.printf("getInputCurrentLimit: %d mA\n", PPM.getInputCurrentLimit());
-        //PPM.disableCurrentLimitPin();
-        //PPM.setChargeTargetVoltage(4208);
-        //PPM.setPrechargeCurr(64);
-       // PPM.setChargerConstantCurr(832);
-       // PPM.getChargerConstantCurr();
-        //Serial.printf("getChargerConstantCurr: %d mA\n", PPM.getChargerConstantCurr());
-       // PPM.enableMeasure(PowersBQ25896::CONTINUOUS);
-       // PPM.disableOTG();
-       // PPM.enableCharge();
-    }
-//}
+}
+
 bool isCharging() {
-    // PPM.disableBatterPowerPath();
-  //  return PPM.isCharging();
     return false;
 }
 
 int getBattery() {
-    //int voltage = PPM.getBattVoltage();
-   // int percent = (voltage - 3300) * 100 / (float)(4150 - 3350);
-
-  //  if (percent < 0) return 1;
- //   if (percent > 100) percent = 100;
-
-  //  if (PPM.isCharging() && percent >= 97) {
-    //    PPM.disableBatLoad();
-    //    percent = 95; // estimate still charging
-  //  }
-
-    //if (PPM.isChargeDone()) { percent = 100; }
-
-    //return percent;
     return 100;
 }
 
@@ -126,7 +81,7 @@ void InputHandler(void) {
 /*********************************************************************
 ** Function: powerOff
 ** location: mykeyboard.cpp
-** Turns off the device (or try to)
+** Turns off the device
 **********************************************************************/
 void powerOff() {
     esp_sleep_enable_ext0_wakeup((gpio_num_t)SEL_BTN, BTN_ACT);
@@ -136,7 +91,7 @@ void powerOff() {
 /*********************************************************************
 ** Function: checkReboot
 ** location: mykeyboard.cpp
-** Btn logic to turn off the device (name is odd btw)
+** Btn logic to turn off the device
 **********************************************************************/
 void checkReboot() {
     int countDown = 0;
@@ -144,7 +99,6 @@ void checkReboot() {
     if (digitalRead(L_BTN) == BTN_ACT && digitalRead(R_BTN) == BTN_ACT) {
         uint32_t time_count = millis();
         while (digitalRead(L_BTN) == BTN_ACT && digitalRead(R_BTN) == BTN_ACT) {
-            // Display poweroff bar only if holding button
             if (millis() - time_count > 500) {
                 if (countDown == 0) {
                     int textWidth = tft.textWidth("PWR OFF IN 3/3", 1);
@@ -165,7 +119,6 @@ void checkReboot() {
             }
         }
 
-        // Clear text after releasing the button
         delay(30);
         if (millis() - time_count > 500) {
             tft.fillRect(60, 12, tftWidth - 60, tft.fontHeight(1), bruceConfig.bgColor);
